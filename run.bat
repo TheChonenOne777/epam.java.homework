7e42349c52c63ee87506247de6b5d3955f632db1
TheChonenOne777 <vov1992@yahoo.com>
2017-05-03 16:07:30 +0300

    added edited src files

--- run.bat
+++
@@ -1,2 +0,0 @@
-javac -d bin -cp .;src\javase01\t01\logic src\javase01\t01\main\Main.java
-java -cp .\bin javase01.t01.main.Main
---
+++ src/main/resources/run.bat
@@ -0,0 +1,2 @@
+javac -d bin -cp .;src\javase01\t01\logic src\javase01\t01\main\Main.java
+java -cp .\bin javase01.t01.main.Main
