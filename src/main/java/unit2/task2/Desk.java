3038fca8a2b5ace351a46ef721849787a97661b4
TheChonenOne777 <vov1992@yahoo.com>
2017-05-11 16:59:09 +0300

    added functional to Desk and Item classes

--- src/main/java/unit2/task2/Desk.java
+++ src/main/java/unit2/task2/Desk.java
@@ -49,7 +49,49 @@ public class Desk {
 
 
     public void addItem(Item item){
-
+        items.add(item);
     }
 
+    public void removeItem(Item item){
+        items.remove(item);
+    }
+
+    public int countTotal(){
+        int total = 0;
+
+        for(Item item : items){
+            total += item.getPrice();
+        }
+
+        return total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Desk desk = (Desk) o;
+
+        if (id != desk.id) return false;
+        if (owner != null ? !owner.equals(desk.owner) : desk.owner != null) return false;
+        return items != null ? items.equals(desk.items) : desk.items == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (owner != null ? owner.hashCode() : 0);
+        result = 31 * result + (items != null ? items.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Desk{" +
+                "id=" + id +
+                ", owner='" + owner + '\'' +
+                ", items=" + items +
+                '}';
+    }
 }
--- src/main/java/unit2/task2/Item.java
+++ src/main/java/unit2/task2/Item.java
@@ -1,11 +1,35 @@
 package unit2.task2;
 
-/**
- * Created by chertvl on 5/11/2017.
- */
 public class Item {
 
-    private String name;
-    private int price;
+    private String name = "";
+    private int price = 0;
+
+    public Item(){};
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) throws Exception {
+        if(price > 0) {
+            this.price = price;
+        } else {
+            throw new Exception();
+        }
+    }
 
 }
