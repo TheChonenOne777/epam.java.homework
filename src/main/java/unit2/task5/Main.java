5785a00ddbeeaea60cffd4ddf86a5b968363a004
TheChonenOne777 <vov1992@yahoo.com>
2017-05-15 19:02:21 +0300

    add task5

---
+++ src/main/java/unit2/task5/Main.java
@@ -0,0 +1,29 @@
+package unit2.task5;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NuclearSubmarine nuclearSubmarine = new NuclearSubmarine();
+
+        System.out.println("Try to speed up with engine off");
+        nuclearSubmarine.speedUp(10);
+        System.out.println("Is engine running: " + nuclearSubmarine.engine.isIfOn());
+
+        System.out.println();
+
+        System.out.println("Starting engine");
+        nuclearSubmarine.engine.runEngine();
+        System.out.println("Is engine running: " + nuclearSubmarine.engine.isIfOn());
+
+        System.out.println();
+
+        System.out.println("Speed up a submarine");
+        nuclearSubmarine.speedUp(10);
+        System.out.println("Submarine speed is " + nuclearSubmarine.getSpeed());
+
+
+    }
+
+}
---
+++ src/main/java/unit2/task5/NuclearSubmarine.java
@@ -0,0 +1,50 @@
+package unit2.task5;
+
+
+public class NuclearSubmarine {
+
+    private int speed;
+    protected NuclearSubmarineEngine engine;
+
+    public NuclearSubmarine(){
+        speed = 0;
+        engine = new NuclearSubmarineEngine();
+    }
+
+    class NuclearSubmarineEngine{
+        private boolean ifOn;
+
+        private NuclearSubmarineEngine(){
+            ifOn = false;
+        }
+
+        protected void runEngine(){
+            ifOn = true;
+        }
+
+        public boolean isIfOn(){
+            return ifOn;
+        }
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void speedUp(int speedUp){
+        if(engine.ifOn) {
+            speed += speedUp;
+        } else {
+            System.out.println("Engine should be on!");
+        }
+    }
+
+    public void slowDown(int speedDown){
+        if(engine.ifOn) {
+            speed -= speedDown;
+        } else {
+            System.out.println("Engine should be on!");
+        }
+    }
+
+}
