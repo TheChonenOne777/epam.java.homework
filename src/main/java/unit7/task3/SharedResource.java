7c45845f8b1edea161fc220e45fcd1765bcec5a2
thechosenone777 <vov1992@yahoo.com>
2017-07-07 17:45:27 +0300

    added unit7 task 3

---
+++ src/main/java/unit7/task3/SharedResource.java
@@ -0,0 +1,27 @@
+package unit7.task3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SharedResource {
+    private List<Integer> list;
+
+    public SharedResource() {
+        list = new ArrayList<Integer>();
+    }
+
+    public void setElement(Integer element) {
+        list.add(element);
+    }
+
+    public Integer getELement() {
+        if (list.size() > 0) {
+            return list.remove(0);
+        }
+        return null;
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+}
